d97f37939e8359fcf26368e4a3bcdbcf4fc9248e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-04 18:47:01 +0300

    Add docstring for class method format and __init__

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,10 +14,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """Initializer"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Obfuscate fields with format"""
         message = super(RedactingFormatter, self).format(record)
         result = filter_datum(
             self.fields, self.REDACTION, message, self.SEPARATOR)
