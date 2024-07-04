44f00adb7301848279aa98fe684339e0f4d1ee5e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-04 16:09:02 +0300

    Change approach

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,9 +10,6 @@ def filter_datum(fields: List[str],
                  separator: str) -> str:
     """
     Returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(
-            rf'({re.escape(field)}=)[^{re.escape(separator)}]*',
-            r'\1' + redaction,
-            message)
-    return message
+    pattern = '|'.join([f"{field}=[^{separator}]+" for field in fields])
+    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={redaction}",
+                  message)
