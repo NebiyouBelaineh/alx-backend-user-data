61c618a90f825a8cafee10b911fbe8c5c2f9960c
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-04 15:22:54 +0300

    Add solution for task 1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module containing filtered_datum function"""
+import logging
+import re
+from typing import Union, List
+
+
+def filter_datum(flds: List[str], rdcn: str, msg: str, sep: str) -> str:
+    """Returns the log messafe obfuscated"""
+    pattern = '|'.join([f"{field}=[^{sep}]+" for field in flds])
+    return re.sub(pattern, lambda m: f"{m.group(0).split('=')[0]}={rdcn}", msg)
