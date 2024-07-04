ac8be8b02c71c17f00fa0c38c1ec06f489c342f6
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-04 20:32:58 +0300

    Add solution for task 3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Module containing filtered_datum function"""
 import re
-from typing import List
 import logging
+import os
+import mysql.connector
+from typing import List
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
@@ -48,3 +50,20 @@ def get_logger() -> logging.Logger:
     logger.propagate = False
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database"""
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+
+    db = mysql.connector.connect(
+        host=host,
+        database=name,
+        user=user,
+        password=pwd,
+        port=3306,
+    )
+    return db
