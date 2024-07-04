c2d32374be350c28abecf4faca04d2faa91eeefa
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-04 15:34:16 +0300

    Remove unnecessary Import

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module containing filtered_datum function"""
 import re
-
+from typing import List
 
 def filter_datum(flds: List[str], rdcn: str, msg: str, sep: str) -> str:
     """Returns the log messafe obfuscated"""
