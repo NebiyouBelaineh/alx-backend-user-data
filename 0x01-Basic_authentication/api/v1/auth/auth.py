4d9e832ae0f7d987aad4bcdb0a7873ce99af7199
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-08 15:58:24 +0300

    Add Auth class defintion

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Authentication Class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user"""
+        return None
