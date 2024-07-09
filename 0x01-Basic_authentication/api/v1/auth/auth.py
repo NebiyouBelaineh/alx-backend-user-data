0047fbb544e686e4f7754f44274491cd1c669418
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-09 13:53:55 +0300

    Improve require_auth in class Auth to allow '*' at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,6 +15,8 @@ class Auth:
             return True
         if path in excluded_paths or path + '/' in excluded_paths:
             return False
+        if path in excluded_paths or path + '*' in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
