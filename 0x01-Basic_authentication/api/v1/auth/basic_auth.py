da787e18461894d6c173d9ecbf124c02f4c67a6a
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-09 09:55:50 +0300

    Add decode_base64_authorization_header method for BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -18,3 +18,21 @@ class BasicAuth(Auth):
             return None
         auth_scheme, auth_params = authorization_header.split(' ', maxsplit=1)
         return auth_params
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str)\
+            -> str:
+        """Returns the decoded value of a Base64
+        string base64_authorization_header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            if len(base64_authorization_header) % 4 != 0:
+                raise Exception
+            auth_header = base64.b64decode(base64_authorization_header,
+                                           validate=True)
+            return auth_header.decode('utf-8')
+        except Exception:
+            return None
