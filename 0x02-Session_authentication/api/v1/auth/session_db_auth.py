8c40c36fc2581035f118265940ee096d216b66f4
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-11 21:10:52 +0300

    Add solution for task 10

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Module containing SessionDBAuth class"""
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from models.user_session import UserSession
+from datetime import datetime, timedelta
+
+
+class SessionDBAuth(SessionExpAuth):
+    """An authentication class that inherits from SessionExpAuth"""
+    def create_session(self, user_id: str = None) -> str:
+        """Creates and returns a session ID for a user_id."""
+        session_id = super().create_session(user_id)
+        if type(session_id) == str:
+            kwargs = {
+                'user_id': user_id,
+                'session_id': session_id,
+            }
+            user_session = UserSession(**kwargs)
+            user_session.save()
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Gets the user id of a User based on a session id"""
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return None
+        if len(sessions) <= 0:
+            return None
+        cur_time = datetime.now()
+        time_span = timedelta(seconds=self.session_duration)
+        exp_time = sessions[0].created_at + time_span
+        if exp_time < cur_time:
+            return None
+        return sessions[0].user_id
+
+    def destroy_session(self, request=None) -> bool:
+        """Destroys a session.
+        """
+        session_id = self.session_cookie(request)
+        try:
+            sessions = UserSession.search({'session_id': session_id})
+        except Exception:
+            return False
+        if len(sessions) <= 0:
+            return False
+        sessions[0].remove()
+        return True
