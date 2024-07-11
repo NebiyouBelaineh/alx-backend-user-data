1caa377bc386590b3fb34d1cf653cc0d505fd837
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-11 15:17:02 +0300

    Fix/ Set-cookie with name according to the env var SESSION_NAME and value to the newly created session

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import abort, request, jsonify
 from models.user import User
+import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -12,10 +13,12 @@ def login() -> str:
     returns a dictionary of the User
     Otherwise, returns a corosponding error message inside a JSON"""
     email, pwd = request.form.get('email'), request.form.get('password')
+
     if email is None or email is '':
         return jsonify({"error": "email missing"}), 400
     if pwd is None or pwd is '':
         return jsonify({"error": "password missing"}), 400
+
     try:
         user_list = User.search({'email': email})
     except Exception:
@@ -27,4 +30,8 @@ def login() -> str:
         return jsonify({"error": "wrong password"}), 401
     from api.v1.app import auth
     session_id = auth.create_session(user.id)
-    return user.to_json({"user_id": user.id, "session_id": session_id})
+    response = jsonify(user.to_json())
+    cookie_name = os.getenv('SESSION_NAME')
+
+    response.set_cookie(cookie_name, session_id)
+    return response
