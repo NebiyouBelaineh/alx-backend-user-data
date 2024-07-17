a35958bf67905cf87e82518f5bac195bdd65cb6e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-17 11:31:25 +0300

    Add DB class and implement add_user method

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 """Module containing User model, an SQLALCHEMY model"""
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Optional
+
+Base = declarative_base()
 
 
-class User:
+class User(Base):
     """User Model"""
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __init__(self, email: str, hashed_password: str,
+                 session_id: Optional[str] = None,
+                 reset_token: Optional[str] = None):
+        """Initialize"""
+        self.email = email
+        self.hashed_password = hashed_password
+        self.session_id = session_id
+        self.reset_token = reset_token
