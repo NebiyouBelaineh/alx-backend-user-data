c325a9692fd271a21fa8eba43790ca5097a7d85f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-17 18:03:09 +0300

    Test type annotation check

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def _hash_password(password: str) -> bytes:
+def _hash_password(password: str):
     """Hash a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
