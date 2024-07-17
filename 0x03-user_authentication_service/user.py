9143cec83db5434a39a37b7448516a3555c75f3d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-17 10:22:25 +0300

    Add User model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module containing User model, an SQLALCHEMY model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User Model"""
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hased_password = Column(String(250), nullable=False)
+    session_id= Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
