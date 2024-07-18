05d1a289f5de8bf07eca9edb2428c5c1ade2259e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-18 19:05:36 +0300

    Add Auth.destroy_session

--- 0x03-user_authentication_service/main8.py
+++ 0x03-user_authentication_service/main8.py
@@ -3,13 +3,31 @@
 Main file
 """
 from auth import Auth
+from time import sleep
 
 email = 'bob@bob.com'
 password = 'MyPwdOfBob'
 auth = Auth()
 
-auth.register_user(email, password)
+print("Testing create_session...")
+sleep(2)
 
-print(auth.create_session(email))
-print(auth.get_user_from_session_id(auth.create_session(email)))
+user_1 = auth.register_user(email, password)
+session_id = auth.create_session(email)
+print(f"user_1 sessiond_id: {session_id}")
+
+
+user_2 = auth.register_user(email+"2", password)
+session_id = auth.create_session(email+"2")
+print(f"user_2 sessiond_id: {session_id}")
+
+
 print(auth.create_session("unknown@email.com"))
+
+print("Testing get_user_from_session_id...")
+sleep(2)
+
+session_id = (auth.create_session(email))
+print(f"user from session_id: {session_id} is: {auth.get_user_from_session_id(session_id).id}")
+
+print(auth.destroy_session(auth.get_user_from_session_id(auth.create_session(email)).id))
