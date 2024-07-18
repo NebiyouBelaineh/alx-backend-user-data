3c51ae16c779a742b0611eefaf5c4ec91a1730c9
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-18 14:45:13 +0300

    Add route to handle adding users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
+from auth import Auth
+from typing import Union, Tuple
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", strict_slashes=False)
 def home() -> Response:
     """Returns JSON Payload"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> Union[Response, Tuple[Response, int]]:
+    """Gets email and password from the form and checks if it's valid
+    and registers it if it does not exist"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)  # type: ignore
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
