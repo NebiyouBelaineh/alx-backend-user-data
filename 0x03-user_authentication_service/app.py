76bb53184a3ed423145efb3a387ad6d21c1a39f5
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-18 15:30:11 +0300

    Add /sessions route to login user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify, Response, request
+from flask import Flask, jsonify, Response, request, abort
 from auth import Auth
 from typing import Union, Tuple
 
@@ -25,6 +25,21 @@ def users() -> Union[Response, Tuple[Response, int]]:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> Union[Response, Tuple[Response, int]]:
+    """Checks email and password and creates session ID and returns
+    a JSON payload with the 'session_id' set as a cookie in the
+    response object"""
+    email, pwd = request.form.get("email"), request.form.get("password")
+    user = AUTH.valid_login(email, pwd)  # type: ignore
+    if user:
+        session_id = AUTH.create_session(email)  # type: ignore
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)  # type: ignore
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
