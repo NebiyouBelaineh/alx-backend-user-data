d2fc08e901fa3c969270748fe8f729f0421bca39
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-07-18 14:09:18 +0300

    Add Auth.register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@
 """Auth Module"""
 import bcrypt
 from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
@@ -10,6 +12,17 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user
+        """
+        try:  # if user found, will raise an ValueError exception
+            user = self._db.find_user_by(email=email)
+            raise ValueError("User <user's email> already exists")
+        except NoResultFound:
+            user = self._db.add_user(email, str(_hash_password(password)))
+            return user
+
 
 def _hash_password(password: str) -> bytes:
     """Hash a password
--- 0x03-user_authentication_service/main5.py
+++ 0x03-user_authentication_service/main5.py
@@ -5,3 +5,4 @@ Main file
 from auth import _hash_password
 
 print(_hash_password("Hello Holberton"))
+print(type(_hash_password("Hello Holberton")))
